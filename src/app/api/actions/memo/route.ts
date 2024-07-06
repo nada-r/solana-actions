e8262fda623fab214fceba218c3a9336c59180e2
nada-r <nadarzer@gmail.com>
2024-07-06 15:09:37 +0100

    create blink transaction

--- src/app/api/actions/memo/route.ts
+++ src/app/api/actions/memo/route.ts
@@ -1,4 +1,5 @@
-import { ACTIONS_CORS_HEADERS, ActionGetResponse } from "@solana/actions"
+import { ACTIONS_CORS_HEADERS, ActionGetResponse, MEMO_PROGRAM_ID } from "@solana/actions"
+import { Transaction, TransactionInstruction, PublicKey, ComputeBudgetProgram } from "@solana/web3.js"
 
 export const GET = (req: Request) => {
 
@@ -19,6 +20,23 @@ export const OPTIONS = GET;
 
 export const POST = (req: Request) => {
     try {
+
+        const transaction = new Transaction();
+
+        transaction.add(
+            // note: `createPostResponse` requires at least 1 non-memo instruction
+            ComputeBudgetProgram.setComputeUnitPrice({
+                microLamports: 1000,
+            }),
+        transaction.add(new TransactionInstruction({
+            programId: new PublicKey(MEMO_PROGRAM_ID),
+            data: Buffer.from("This is a simple message", "utf8"),
+            keys: []
+        }),
+    );
+    } catch (err) {
+
+
     } catch (err) {
         return Response.json("An unknown error occured", {status:400});
     }
