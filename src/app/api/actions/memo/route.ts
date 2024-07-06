1f41f79d6058b650bb731d8b34b8c6fd5c468838
nada-r <nadarzer@gmail.com>
2024-07-06 14:45:56 +0100

    setup ActiongetResponse and erro handling for POSt request

--- src/app/api/actions/memo/route.ts
+++ src/app/api/actions/memo/route.ts
@@ -1,3 +1,25 @@
-export const GET = () => {
-    
-}
+import { ACTIONS_CORS_HEADERS, ActionGetResponse } from "@solana/actions"
+
+export const GET = (req: Request) => {
+
+    const payload : ActionGetResponse = {
+        icon: new URL("/solana_devs.jpeg", new URL(req.url).origin).toString(),
+        label: "Send Memo",
+        description: "Follow the alpha",
+        title: "Here is my call"
+
+    }
+
+    return Response.json(payload, {
+        headers: ACTIONS_CORS_HEADERS
+    });
+};
+
+export const OPTIONS = GET;
+
+export const POST = (req: Request) => {
+    try {
+    } catch (err) {
+        return Response.json("An unknown error occured", {status:400});
+    }
+};
